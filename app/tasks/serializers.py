ca397db496f57997268df00dd4bb153e1a21d813
paradisomartin <martinparadiso024@gmail.com>
2024-09-12 18:45:34 -0300

    api ready

--- app/tasks/serializers.py
+++ app/tasks/serializers.py
@@ -7,44 +7,34 @@ class TagSerializer(serializers.ModelSerializer):
         fields = ['id', 'title']
         read_only_fields = ['id']
 
-class TagListField(serializers.ListField):
-    child = serializers.CharField()
-
 class TaskSerializer(serializers.ModelSerializer):
-    tags = serializers.ListField(child=serializers.CharField(), required=False, write_only=True)
-    tag_titles = serializers.SerializerMethodField(read_only=True)
+    tags = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='title'
+    )
 
     class Meta:
         model = Task
-        fields = ['id', 'title', 'description', 'created_at', 'tags', 'tag_titles']
+        fields = ['id', 'title', 'description', 'created_at', 'is_completed', 'tags']
         read_only_fields = ['id', 'created_at']
 
     def create(self, validated_data):
-        tags_data = validated_data.pop('tags', [])
-        if 'description' not in validated_data:
-            validated_data['description'] = ''
+        tags_data = self.context['request'].data.get('tags', [])
         task = Task.objects.create(user=self.context['request'].user, **validated_data)
         for tag_title in tags_data:
             tag, _ = Tag.objects.get_or_create(title=tag_title, user=task.user)
             task.tags.add(tag)
         return task
 
-    def get_tag_titles(self, obj):
-        return [tag.title for tag in obj.tags.all()]
-
     def update(self, instance, validated_data):
-        tag_names = validated_data.pop('tags', None)
+        tags_data = self.context['request'].data.get('tags', None)
         instance = super().update(instance, validated_data)
 
-        if tag_names is not None:
+        if tags_data is not None:
             instance.tags.clear()
-            for tag_name in tag_names:
-                tag, _ = Tag.objects.get_or_create(user=instance.user, title=tag_name)
+            for tag_title in tags_data:
+                tag, _ = Tag.objects.get_or_create(user=instance.user, title=tag_title)
                 instance.tags.add(tag)
 
         return instance
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['tags'] = [tag.title for tag in instance.tags.all()]
-        return representation
--- app/todolist/settings.py
+++ app/todolist/settings.py
@@ -157,8 +157,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
-IMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
@@ -168,6 +168,6 @@ IMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
     'USER_ID_FIELD': 'id',
     'USER_ID_CLAIM': 'user_id',
-    'AUTH_TOKEN_CLASSES': ('access',),
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
     'TOKEN_TYPE_CLAIM': 'token_type',
 }
